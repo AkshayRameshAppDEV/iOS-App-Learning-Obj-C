fd1810145ced6ff056f372ac10347f6ac920e5a0
Akshay Ramesh <akshay.ramesh@zimperium.com>
2020-02-20 13:47:31 -0600

    Playing with Storyboard drag and drop contents

---
+++ MyFirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstApp
+//
+//  Created by Akshay Ramesh on 2/19/20.
+//  Copyright © 2020 Akshay Ramesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyFirstApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MyFirstApp
+//
+//  Created by Akshay Ramesh on 2/19/20.
+//  Copyright © 2020 Akshay Ramesh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
